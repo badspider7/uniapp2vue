85e96ce8b0df4e112fb6258d99364f2cbb54673f
yellowapple <1742969779@qq.com>
2023-11-09 18:54:40 +0800

    feat:文件拷贝到src

---
+++ index.js
@@ -0,0 +1,37 @@
+//创建文件夹
+// fs.ensureDir("./src/copy")
+//   .then(() => {
+//     console.log("mkdir-success");
+//   })
+//   .catch((err) => {
+//     console.log("mkdir-error", err);
+//   });
+
+//创建文件
+// fs.ensureFile("./src/main.js", (err) => {
+//   if (err) {
+//     console.log("ensureFile-error", err);
+//   }
+// });
+
+//删除文件夹
+// fs.remove("./src", (err) => {
+//   if (err) {
+//     console.log("remove-err", err);
+//   }
+// });
+
+//拷贝文件或文件夹
+// fs.copy("./src", "./test")
+//   .then(() => {
+//     console.log("copy success");
+//   })
+//   .catch((err) => {
+//     console.log("copy err", err);
+//   });
+
+// fs.ensureFile("./.gitignore", (err) => {
+//   if (err) {
+//     console.log("remove-err", err);
+//   }
+// });
